cb3cd68bec1d7a312bb6cc2f198e3c33c536281c
Marlon Rouse <marlon.rouse@gmail.com>
2015-12-18 20:29:05 +1100

    code for wear

---
+++ wear/src/main/java/com/example/android/sunshine/app/MyService.java
@@ -0,0 +1,26 @@
+package com.example.android.sunshine.app;
+
+import android.util.Log;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MyService extends WearableListenerService {
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent dataEvent: dataEvents) {
+            if (dataEvent.getType()==DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/step-counter")) {
+                    int steps = dataMap.getInt("step-count");
+                    Log.d("DEBUG", "successfully sent. steps = "+String.valueOf(steps));
+                }
+            }
+        }
+    }
+}
